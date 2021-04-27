6d7df0bfb5724486f64b00867fc6161d5d41ac3e
Tomasz Pęczek <tpeczek@gmail.com>
2021-04-27 21:32:51 +0200

    Adding HTTP content based on NDJSON and IAsyncEnumerable<T>

---
+++ src/Ndjson.AsyncStreams.Net.Http/NdjsonAsyncEnumerableContent.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Ndjson.AsyncStreams.Net.Http
+{
+    /// <summary>
+    /// Provides HTTP content based on NDJSON and <see cref="IAsyncEnumerable{T}"/>.
+    /// </summary>
+    public class NdjsonAsyncEnumerableContent : HttpContent
+    {
+        private static readonly byte[] _newlineDelimiter = Encoding.UTF8.GetBytes("\n");
+
+#if NETCOREAPP3_1
+        private static readonly JsonSerializerOptions _defaultJsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy .CamelCase
+        };
+#endif
+
+#if NET5_0
+        private static readonly JsonSerializerOptions _defaultJsonSerializerOptions = new(JsonSerializerDefaults.Web);
+#endif
+
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        /// <summary>
+        /// Gets the type of the values in async stream to be serialized by this instance.
+        /// </summary>
+        public Type ValuesType { get; }
+
+        /// <summary>
+        /// Gets the async stream of values to be serialized and used as the body of the <see cref="HttpRequestMessage"/> that sends this instance.
+        /// </summary>
+        public IAsyncEnumerable<object> Values { get;  }
+
+        private NdjsonAsyncEnumerableContent(IAsyncEnumerable<object> values, Type valuesType, JsonSerializerOptions? jsonSerializerOptions = null)
+        {
+            Values = values ?? throw new ArgumentNullException(nameof(values));
+            ValuesType = valuesType ?? throw new ArgumentNullException(nameof(valuesType));
+
+            _jsonSerializerOptions = jsonSerializerOptions ?? _defaultJsonSerializerOptions;
+
+            Headers.ContentType = new MediaTypeHeaderValue("application/x-ndjson")
+            {
+                CharSet = Encoding.UTF8.WebName
+            };
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="NdjsonAsyncEnumerableContent"/> class that will contain the values async stream serialized as NDJSON.
+        /// </summary>
+        /// <typeparam name="T">The type of the values in async stream to be serialized.</typeparam>
+        /// <param name="values">The async stream of values to be serialized.</param>
+        /// <param name="options">The options to control the behavior during serialization.</param>
+        /// <returns>A <see cref="NdjsonAsyncEnumerableContent"/> instance.</returns>
+        public static NdjsonAsyncEnumerableContent Create<T>(IAsyncEnumerable<T> values, JsonSerializerOptions? options = null)
+            => new ((IAsyncEnumerable<object>)values, typeof(T), options);
+
+        /// <inheritdoc/>
+        protected override async Task SerializeToStreamAsync(Stream stream, TransportContext? context)
+        {
+            await foreach (object value in Values.ConfigureAwait(false))
+            {
+                await JsonSerializer.SerializeAsync(stream, value, ValuesType, _jsonSerializerOptions).ConfigureAwait(false);
+                await stream.WriteAsync(_newlineDelimiter).ConfigureAwait(false);
+                await stream.FlushAsync().ConfigureAwait(false);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override bool TryComputeLength(out long length)
+        {
+            length = -1;
+
+            return false;
+        }
+    }
+}
---
+++ test/Ndjson.AsyncStreams.Net.Http.Tests/Unit/NdjsonAsyncEnumerableContentTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Ndjson.AsyncStreams.Net.Http.Tests.Unit
+{
+    public class NdjsonAsyncEnumerableContentTests
+    {
+        private const string SERIALIZED_VALUES = "{\"id\":1,\"name\":\"Value 01\"}\n{\"id\":2,\"name\":\"Value 02\"}\n";
+
+        private class ValueType
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        private async IAsyncEnumerable<ValueType> GetValuesAsync()
+        {
+            await Task.CompletedTask;
+
+            yield return new ValueType { Id = 1, Name = "Value 01" };
+
+            await Task.CompletedTask;
+
+            yield return new ValueType { Id = 2, Name = "Value 02" };
+        }
+
+        [Fact]
+        public void Create_ValuesType_IsDeclaredType()
+        {
+            IAsyncEnumerable<ValueType> values = GetValuesAsync();
+
+            NdjsonAsyncEnumerableContent ndjsonAsyncEnumerableContent = NdjsonAsyncEnumerableContent.Create(values);
+
+            Assert.Equal(typeof(ValueType), ndjsonAsyncEnumerableContent.ValuesType);
+        }
+
+        [Fact]
+        public void Create_Values_IsProvidedValues()
+        {
+            IAsyncEnumerable<ValueType> values = GetValuesAsync();
+
+            NdjsonAsyncEnumerableContent ndjsonAsyncEnumerableContent = NdjsonAsyncEnumerableContent.Create(values);
+
+            Assert.Same(values, ndjsonAsyncEnumerableContent.Values);
+        }
+
+        [Fact]
+        public void Create_MediaType_IsNdjson()
+        {
+            IAsyncEnumerable<ValueType> values = GetValuesAsync();
+
+            NdjsonAsyncEnumerableContent ndjsonAsyncEnumerableContent = NdjsonAsyncEnumerableContent.Create(values);
+
+            Assert.Equal("application/x-ndjson", ndjsonAsyncEnumerableContent.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public void Create_CharSet_IsUtf8()
+        {
+            IAsyncEnumerable<ValueType> values = GetValuesAsync();
+
+            NdjsonAsyncEnumerableContent ndjsonAsyncEnumerableContent = NdjsonAsyncEnumerableContent.Create(values);
+
+            Assert.Equal("utf-8", ndjsonAsyncEnumerableContent.Headers.ContentType.CharSet);
+        }
+        
+        [Fact]
+        public async Task ReadAsStringAsync_ReturnsCorrectNdjson()
+        {
+            IAsyncEnumerable<ValueType> values = GetValuesAsync();
+
+            NdjsonAsyncEnumerableContent ndjsonAsyncEnumerableContent = NdjsonAsyncEnumerableContent.Create(values);
+
+            Assert.Equal(SERIALIZED_VALUES, await ndjsonAsyncEnumerableContent.ReadAsStringAsync());
+        }
+    }
+}
