287eee99113ad995b8a3cb8ee79197337d20226a
Tomasz Pęczek <tpeczek@gmail.com>
2021-07-29 21:05:58 +0200

    Cache reflected AsyncEnumerableModelReader<T> (#1)

--- src/Ndjson.AsyncStreams.AspNetCore.Mvc.NewtonsoftJson/Formatters/NewtonsoftNdjsonInputFormatter.cs
+++ src/Ndjson.AsyncStreams.AspNetCore.Mvc.NewtonsoftJson/Formatters/NewtonsoftNdjsonInputFormatter.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Newtonsoft.Json;
@@ -43,6 +44,7 @@ namespace Ndjson.AsyncStreams.AspNetCore.Mvc.NewtonsoftJson.Formatters
 
         private static readonly Type _asyncEnumerableType = typeof(IAsyncEnumerable<>);
 
+        private readonly ConcurrentDictionary<Type, IAsyncEnumerableModelReader?> _asyncEnumerableModelReaders = new();
         private readonly ILogger<NewtonsoftNdjsonInputFormatter> _logger;
 
         /// <inheritdoc />
@@ -91,12 +93,11 @@ namespace Ndjson.AsyncStreams.AspNetCore.Mvc.NewtonsoftJson.Formatters
                 throw new ArgumentNullException(nameof(encoding));
             }
 
-            Type modelReaderType = typeof(AsyncEnumerableModelReader<>).MakeGenericType(context.ModelType.GetGenericArguments()[0]);
-            IAsyncEnumerableModelReader? modelReader = (IAsyncEnumerableModelReader?)Activator.CreateInstance(modelReaderType);
+            IAsyncEnumerableModelReader? modelReader = GetModelReader(context.ModelType);
 
             if (modelReader is null)
             {
-                string errorMessage = $"Couldn't create an instance of {modelReaderType.Name} for deserializing incoming async stream.";
+                string errorMessage = $"Couldn't create an instance of {nameof(IAsyncEnumerableModelReader)} for deserializing incoming async stream.";
 
                 _logger.LogDebug(errorMessage);
                 context.ModelState.TryAddModelError(String.Empty, errorMessage);
@@ -105,6 +106,20 @@ namespace Ndjson.AsyncStreams.AspNetCore.Mvc.NewtonsoftJson.Formatters
             }
 
             return Task.FromResult(InputFormatterResult.Success(modelReader.ReadModel(context.HttpContext.Request.Body, SerializerSettings)));
+        }
+
+        private IAsyncEnumerableModelReader? GetModelReader(Type modelType)
+        {
+            IAsyncEnumerableModelReader? modelReader;
+            if (!_asyncEnumerableModelReaders.TryGetValue(modelType, out modelReader))
+            {
+                Type modelReaderType = typeof(AsyncEnumerableModelReader<>).MakeGenericType(modelType.GetGenericArguments()[0]);
+                modelReader = (IAsyncEnumerableModelReader?)Activator.CreateInstance(modelReaderType);
+
+                _asyncEnumerableModelReaders.TryAdd(modelType, modelReader);
+            }
+
+            return modelReader;
         }
     }
 }
