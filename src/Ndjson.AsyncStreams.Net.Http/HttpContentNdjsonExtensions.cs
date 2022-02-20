5725631ec2d23738b79dcb5471615eebfc877535
Tomasz Pęczek <tpeczek@gmail.com>
2022-02-20 18:09:49 +0100

    Adding initial attempt for handling CancellationToken in ReadFromNdjsonAsync (#6)

--- src/Ndjson.AsyncStreams.Net.Http/HttpContentNdjsonExtensions.cs
+++ src/Ndjson.AsyncStreams.Net.Http/HttpContentNdjsonExtensions.cs
@@ -50,16 +50,32 @@ namespace Ndjson.AsyncStreams.Net.Http
             }
 
             JsonSerializerOptions jsonSerializerOptions = options ?? _defaultJsonSerializerOptions;
-
+#if NETCOREAPP3_1
             using Stream contentStream = await content.ReadAsStreamAsync().ConfigureAwait(false);
+#endif
+#if NET5_0 || NET6_0
+            using Stream contentStream = await content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
+#endif
             using StreamReader contentStreamReader = new (contentStream);
 
+
+#if NETCOREAPP3_1  || NET5_0
             string? valueUtf8Json = await contentStreamReader.ReadLineAsync().ConfigureAwait(false);
+#endif
+#if NET6_0
+            string? valueUtf8Json = await contentStreamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false);
+#endif
+
             while (valueUtf8Json is not null)
             {
                 yield return JsonSerializer.Deserialize<T>(valueUtf8Json, jsonSerializerOptions);
 
+#if NETCOREAPP3_1 || NET5_0
                 valueUtf8Json = await contentStreamReader.ReadLineAsync().ConfigureAwait(false);
+#endif
+#if NET6_0
+                valueUtf8Json = await contentStreamReader.ReadLineAsync().WaitAsync(cancellationToken).ConfigureAwait(false);
+#endif
             }
         }
 
